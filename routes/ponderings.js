6d2eac131cefb778c5c0c597e8c370ff9223025f
violet <violet@violetcraze.net>
2021-02-28 18:15:50 -0700

    display working

--- routes/ponderings.js
+++ routes/ponderings.js
@@ -4,14 +4,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  let ponderings;
   Pondering.find({}, (err, results) => {
     if (err) {
       console.error(err);
     }
-    ponderings = results;
+    res.render('ponderings', { ponderings: results });
   });
-  res.render('ponderings', { ponderings: ponderings });
 });
 
 router.get('/new', (req, res, next) => {
